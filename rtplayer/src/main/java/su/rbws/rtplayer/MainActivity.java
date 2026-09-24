642073a6f33683da4791b664058b1852eabf475e
Konstantin Pevtsov <47675852+rbwsok@users.noreply.github.com>
2026-09-24 12:14:43 +0500

    Update MainActivity.java

--- rtplayer/src/main/java/su/rbws/rtplayer/MainActivity.java
+++ rtplayer/src/main/java/su/rbws/rtplayer/MainActivity.java
@@ -194,7 +194,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         filter.addAction(SoundSystemAbstract.ACTION_PLAYFILE);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
-            registerReceiver(broadcastReceiver, filter, RECEIVER_EXPORTED);
+            registerReceiver(broadcastReceiver, filter, Context.RECEIVER_NOT_EXPORTED);
         else
             registerReceiver(broadcastReceiver, filter);
 
